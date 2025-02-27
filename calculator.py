3b4cb1b2c46c151d3161a59bd31cf2f525d89794
Matthew Tirtasaputra <66460738+MatthewTirta@users.noreply.github.com>
2025-02-27 13:09:51 +0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# Write code below 💖
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def exp(a, b):
+    return a ** b
+
+# Test each function
+print("5 + 3 =", add(5, 3))
+print("10 - 4 =", subtract(10, 4))
+print("2 * 6 =", multiply(2, 6))
+print("8 / 2 =", divide(8, 2))
+print("2 ** 3 =", exp(2, 3))
