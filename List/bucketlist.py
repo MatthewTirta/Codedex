7ad80ca61bcd240dd0cf47301b4f6b61561124cb
Matthew Tirtasaputra <66460738+MatthewTirta@users.noreply.github.com>
2025-02-27 12:07:50 +0700

    Create bucketlist.py

---
+++ List/bucketlist.py
@@ -0,0 +1,15 @@
+# Write code below 💖
+
+things_to_do = [
+  'Create the dopest app to trade automatically',
+  'Travel the world with my love one',
+  'Buy dream car (Nissan GTR)',
+  'Live outside Indonesia for a year or a few years',
+  'Read books more often',
+  'Stay Fit',
+  'Open a cozy diner upstate.',
+  'Grow old with no regrets.'
+]
+
+for i in range(len(things_to_do)):
+  print(things_to_do[i])
