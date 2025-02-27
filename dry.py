87d600e79d88bf43937fe6a7ba765f2a7be5becd
Matthew Tirtasaputra <66460738+MatthewTirta@users.noreply.github.com>
2025-02-27 12:26:20 +0700

    Create dry.py

---
+++ dry.py
@@ -0,0 +1,17 @@
+# Write code below 💖
+
+#Function that prints what inside the print function
+print('Hi my name is Matthew')
+
+#Function that prints how long is the length of the string
+print(len('ABCDEFGHIJK'))
+
+#Function that calculates the maximum of two numbers
+print(max(3, 5))
+
+#Function to input a number into a an integer (Can be a flot/string as well)
+num = int(input("Input a Number: "))
+
+#Function to give ASCII number of an codes, characters and symbols
+ascii = input("Input what to convert: ")
+print(ord(ascii))
