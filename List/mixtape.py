3d429023141ab92567a154273f2abc0d383035ad
Matthew Tirtasaputra <66460738+MatthewTirta@users.noreply.github.com>
2025-02-27 12:01:50 +0700

    add mixtape.py

---
+++ List/mixtape.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+
+playlist = ['100 Degrees - Rich Brian',
+'luther - Kendrick Lamar',
+'Thats What I Want - Lil Nas X',
+'Dat Stick - Rich Brian',
+'Circles - Post Malone']
+
+
+for i in range(len(playlist)):
+  print(playlist[i])
